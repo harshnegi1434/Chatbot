58d00008288e86294ff5ac0b6f171e778a31e5b4
Harsh Negi <harshnegi2002@gmail.com>
2022-05-18 22:59:06 +0530

    updated code

--- trainingData.py
+++ trainingData.py
@@ -3,7 +3,6 @@ import json
 import pickle
 import random
 
-
 import nltk
 import numpy as np
 from keras.layers import Dense, Dropout
@@ -11,17 +10,17 @@ from keras.models import Sequential
 from keras.optimizers import gradient_descent_v2
 from nltk.stem import WordNetLemmatizer
 
-# initializing lemmatizer to get stem of words
-lemmatizer = WordNetLemmatizer()
-
 # loading the Dataset : intents.json
-intents = json.loads(open('C:\Programming\prgms\projectI\intents.json').read())
+intents = json.loads(open('intents.json').read())
 
 # creating several list for model training
-words = []                                  # for bow mode/vocabulary for patterns
+words = []                                  # for bow model/vocabulary for patterns
 classes = []                                # for bow model/vocabulary for tags
 documents = []                              # for storing each patterns
 ignore_letters = ['?', '!', ',', '.']       # ignoring letters during model training
+
+# initializing lemmatizer to get stem of words
+lemmatizer = WordNetLemmatizer()
 
 # iterating over all the intents
 for intent in intents['intents']:
@@ -38,12 +37,12 @@ words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_lett
 # sorting the vocab and classes in alphabetical order and taking the set to ensure no duplicate occur
 words = sorted(set(words))
 classes = sorted(set(classes))
-
 print(words)
+print(classes)
 
 # saving the words and classes in a pickle file
-pickle.dump(words, open('C:\Programming\prgms\projectI\words.pkl', 'wb'))
-pickle.dump(classes, open('C:\Programming\prgms\projectI\classes.pkl', 'wb'))
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
 # creating our training data
 training = []
@@ -77,11 +76,11 @@ train_x = list(training[:, 0])
 train_y = list(training[:, 1])
 print("Training Data Created")
 
+
 # Neural Network Model
 ''' creating model - 3 layers. first layer 128 neurons, second layer 64 neurons
     and 3rd layer contains number of neurons equal to number of intents to 
     predict output intent with softmax '''
-
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
@@ -89,14 +88,16 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
+
 # Compiling Model
-''' using Stochastic Gradient Descent with Nesterov accerelated gradient
+''' using Stochastic Gradient Descent with Nesterov accelerated gradient
     gives good result for this model '''
 sgd = gradient_descent_v2.SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 # fitting the saving the model
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('C:\Programming\prgms\projectI\chatbot_model.h5', hist)
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=150, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
 
+print(model.summary())
 print('Model Created')
